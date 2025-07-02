bb0686586409e5d049209af557cbda9cec6877e6
21anurag <gulavaneanurag@gmail.com>
2025-07-02 11:19:00 -0400

    Added the calendar section

--- index.php
+++ index.php
@@ -6,7 +6,34 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Calendar Project</title>
 
-        
+        <meta name="description" content="Google Calendar Clone">
+
+        <link rel="stylesheet" href="style.css"/>
     </head>
 
+    <body>
+    
+        <header>
+            <h1>🗓️ Calendar</h1>
+        </header>
+
+        <!--Clock-->
+            <div class="clock-container">
+                <div id="clock"></div>
+            </div>
+
+
+        <!--Calendar Section-->
+            <div class="calendar">
+                <div class="nav-btn-container">
+                    <button class="nav-btn">⏪️</button>
+                    <h2 id="monthYear" style="margin: 0"></h2>
+                    <button class="nav-btn">⏩️</button>
+                </div>
+                
+                <div class="calendar-grid" id="calendar"></div>
+            </div>
+    </body>
+
+    
 </html>
