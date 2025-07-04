4e3719d8ece9025fb0f27c6a55aff7fae421f8b8
21anurag <gulavaneanurag@gmail.com>
2025-07-04 11:49:00 -0400

    Added the Main Form section

--- index.php
+++ index.php
@@ -30,10 +30,42 @@
                     <h2 id="monthYear" style="margin: 0"></h2>
                     <button class="nav-btn">⏩️</button>
                 </div>
-                
+
                 <div class="calendar-grid" id="calendar"></div>
             </div>
-    </body>
 
+
+        <!--Model for Add/Edit/Delete Appointment--> 
+         <div id="eventSelectorWrapper">
+            <label for="eventSelector">
+                <strong>Select Event:</strong>
+            </label>    
+            <select id="eventSelector">
+                <option disabled selected>Choose Event...</option>
+            </select>
+         </div>
+
+        <!-- Main Form -->
+         
+         <form action="POST" id="eventForm">
+            <input type="hidden" name="action" id="formAction" value="add">
+            <input type="hidden" name="event_id" id="eventId">
+
+            <label for="eventName">Title</label>
+            <input type="text" name="event_name" id="eventName" required>
+
+            <label for="location">Address</label>
+            <input type="text" name="event_location" id="location" required>
+            
+            <label for="startDate">Start Date:</label>
+            <input type="date" name="start_date" id="startDate" required>
+
+            <label for="endDate">End Date:</label>
+            <input type="date" name="end_date" id="endDate" required>
+
+            <button type="submit">Save</button>
+         </form>
+
+    </body>
     
 </html>
