28643ba1de2ae8cb8476a33647bd06abc720a499
21anurag <gulavaneanurag@gmail.com>
2025-07-05 11:49:00 -0400

    main

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
 
         <meta name="description" content="Google Calendar Clone">
 
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@300&display=swap" rel="stylesheet">
+
         <link rel="stylesheet" href="style.css"/>
     </head>
 
